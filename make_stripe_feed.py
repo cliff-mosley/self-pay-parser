f4a68c3f3cd98f3cf2cc153a05f3a541d639b831
cliff-mosley <82547451+cliff-mosley@users.noreply.github.com>
2021-05-05 18:24:44 -0700

    First commit
    Takes a dynamodb query dump of selfpay data and generates a usable form for consumption by Finance

---
+++ make_stripe_feed.py
@@ -0,0 +1,100 @@
+#! /usr/bin/python3
+# from tabulate import tabulate
+import argparse
+import json
+import logging
+from time import gmtime, strftime
+from dataclasses import dataclass
+
+"""
+    Generate inserts for the month-end stripe feed
+    ** Make this actually run the full process **
+"""
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s', 
+    filename='looker-ext-user.log',
+    encoding='utf-8',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.CRITICAL)
+
+@dataclass
+class SelfPayRecord:
+    reference_id: str
+    current_status: int
+    stripe_price: tuple
+    stripe_price_currency: str
+    stripe_payment_id: str
+    stripe_date: str
+
+def get_json_from_s3(bucket: str):
+    raise NotImplemented
+
+def execute_report_queries(query_list: list[str]):
+    raise NotImplemented
+
+def extract_price(platform_data: dict) -> tuple:
+    # in testing sometimes currency is not returned
+    #   so giving it no value
+
+    try:
+        currency = platform_data['M']['currency']['S']
+    except KeyError:
+        currency = ""
+    amount = float(platform_data['M']['stripePrice']['N'])
+    return (currency, amount)
+
+def convert_epoch_time(i: int) -> str:
+    t = gmtime(int(i))
+    return strftime("%Y-%m-%d", t)
+
+def populate_self_pay_record(d: dict) -> SelfPayRecord:
+    # extract returns a tuple of currency and amount
+    extracted_price = extract_price(d['platformData'])
+    
+    record = SelfPayRecord(
+        d['referenceId']['S'][:8],
+        d['currentStatus']['N'],
+        extracted_price[1], 
+        extracted_price[0],
+        d['stripePaymentId']['S'],
+        convert_epoch_time(d['ts']['N'])
+    )
+    return record
+
+def generate_temp_table_schema():
+    return """create table #tmp (
+                  reference_id int
+                , current_status int
+                , stripe_price numeric(10, 2)
+                , stripe_price_currency nvarchar(3)
+                , stripe_payment_id nvarchar(50),
+                , stripe_date datetime) 
+            go """
+
+def generate_insert_from_records(r: SelfPayRecord) -> str:
+    insert_statement = f"insert into #tmp values ({r.reference_id}, {r.current_status}, {r.stripe_price}, '{r.stripe_price_currency}', '{r.stripe_payment_id}', '{r.stripe_date}'"
+    return insert_statement
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--self_pay_file",
+        help="JSON file containing the records needed in the self-pay feed",
+        required=True)
+
+    args = parser.parse_args()
+
+    print(generate_temp_table_schema())
+    with open(args.self_pay_file) as f:
+        data = json.load(f)
+        
+        extracted_data = data[0]
+        for row in extracted_data:
+            spr = populate_self_pay_record(row)
+            print(generate_insert_from_records(spr))
+
+
+if __name__ == "__main__":
+    main()
